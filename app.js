a97a65e1997cbc09c24de9e4fcf85fd605ce189d
winniehell <git@winniehell.de>
2015-02-19 19:02:13 +0100

    Adjust for Express 3

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var express = require('express-streamline');
 var path = require('path');
 var logger = require('morgan');
 
-var routes = express.Router();
-routes.get('/', function (req, res, _) {
-    res.send(req.app.mountpath + '\n' + req.baseUrl + '\n');
-});
+var middleware = function (req, res, _) {
+    res.send(req.app.route + '\n' + req.originalUrl + '\n');
+};
 
 var subapp = express();
 subapp.use(logger('dev'));
-subapp.use('/', routes);
+subapp.use('/', middleware);
 
 var app = express();
 app.use('/subapp', subapp);
