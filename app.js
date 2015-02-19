a1457fd49bdfd3448cd3c638c663c33fdcf326ac
winniehell <git@winniehell.de>
2015-02-19 18:41:45 +0100

    simplify

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = express.Router();
+routes.get('/', function (req, res, _) {
+    res.send(req.app.mountpath + '\n' + req.baseUrl + '\n');
+});
 
-var app = express();
 var subapp = express();
-app.use('/subapp', subapp);
-
-// view engine setup
-subapp.set('views', path.join(__dirname, 'views'));
-subapp.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 subapp.use(logger('dev'));
-subapp.use(bodyParser.json());
-subapp.use(bodyParser.urlencoded({ extended: false }));
-subapp.use(cookieParser());
-subapp.use(express.static(path.join(__dirname, 'public')));
-
 subapp.use('/', routes);
 
-// catch 404 and forward to error handler
-subapp.use(function(req, res, _) {
-    var err = new Error('Not Found');
-    err.status = 404;
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (subapp.get('env') === 'development') {
-    subapp.use(function(err, req, res, _) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-subapp.use(function(err, req, res, _) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+var app = express();
+app.use('/subapp', subapp);
 
 module.exports = app;
