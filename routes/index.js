2f395bda3c2ffc5f5884fa27a302503e3c3ecfa2
winniehell <git@winniehell.de>
2015-02-19 18:41:45 +0100

    simplify

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, _) {
-    res.send(req.app.mountpath + '\n' + req.baseUrl + '\n');
-});
-
-module.exports = router;
