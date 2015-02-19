9919a2d8ab15f16f543d09093d7c027fbad1ccd4
winniehell <git@winniehell.de>
2015-02-19 20:36:08 +0100

    forward custom error to error handler

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express-streamline');
-var path = require('path');
-var logger = require('morgan');
-
-var routes = express.Router();
-routes.get('/', function (req, res, _) {
-    res.send(req.app.mountpath + '\n' + req.baseUrl + '\n');
-});
-
-var subapp = express();
-subapp.use(logger('dev'));
-subapp.use('/', routes);
+var express = require('express');
 
 var app = express();
-app.use('/subapp', subapp);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('I’m a teapot');
+    err.status = 418;
+    next(err);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send('Error ' + err.status + ': ' + err.message);
+});
 
 module.exports = app;
